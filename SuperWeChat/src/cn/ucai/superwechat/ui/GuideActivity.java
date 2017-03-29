6fd624120d798d34367d91b546a8b305ed744a5e
yu7708 <3173571032@qq.com>
2017-03-30 00:41:47 +0800

    添加欢迎界面

--- SuperWeChat/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,7 +1,16 @@
 package cn.ucai.superwechat.ui;
 
+import android.os.Bundle;
+
+import cn.ucai.superwechat.R;
+
 /**
  * Created by Administrator on 2017/3/30.
  */
-public class GuideActivity {
+public class GuideActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_guide);
+    }
 }
